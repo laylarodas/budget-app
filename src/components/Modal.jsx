7882ce00335d8db907620ef8d309a5c5bf626446
Layla Rodas <rodas.layla@gmail.com>
2024-03-16 10:02:45 +0100

    add states to inputs

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import closeBtn from '../img/close.svg'
 
+
 export const Modal = ({setModal, animateModal, setAnimateModal}) => {
+
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [category, setCategory] = useState('');
 
     const hideModal = () => {
         
@@ -25,17 +30,23 @@ export const Modal = ({setModal, animateModal, setAnimateModal}) => {
             <legend>New Expense</legend>
             <div className='field'>
                 <label htmlFor="name">Expense Name</label>
-                <input type="text" name="name" id="name" placeholder='Introduce the expense name' />
+                <input type="text" name="name" id="name" placeholder='Introduce the expense name' value={name} onChange={(e)=> {
+                    setName(e.target.value)
+                }}/>
 
             </div>
             <div className='field'>
                 <label htmlFor="amount">Amount</label>
-                <input type="number" name="amount" id="amount" placeholder='Introduce amount' />
+                <input type="number" name="amount" id="amount" placeholder='Introduce amount' value={amount} onChange={(e)=> {
+                    setAmount(Number(e.target.value))
+                }}/>
 
             </div>
             <div className='field'>
                 <label htmlFor="category">Category</label>
-                <select name="category" id="category">
+                <select name="category" id="category" value={category} onChange={(e)=> {
+                    setCategory(e.target.value)
+                }} >
                     <option value="">-- Selection --</option>
                     <option value="saving">Saving</option>
                     <option value="food">Food</option>
